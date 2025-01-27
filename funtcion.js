24323d91b3c4da22382e8b0e67af9fb4d118bddf
tim-risch <t.risch@schueler.lg-ks.de>
2025-01-27 15:16:03 +0100

    PopUp (problem), highlighting, pic animation

--- funtcion.js
+++ funtcion.js
@@ -3,3 +3,7 @@ function buttonDown(){
     console.log('Hallo Welt!');
     return 12+13;
 }
+function picture(){
+    var popup = document.getElementByClass("popup");
+    popup.classList.toggle("show");
+}
