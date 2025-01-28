8fe69542bcb0dd66caeb68a061b0d8953c9f9cf0
tim-risch <t.risch@schueler.lg-ks.de>
2025-01-28 15:15:21 +0100

    info box

--- funtcion.js
+++ funtcion.js
@@ -3,7 +3,15 @@ function buttonDown(){
     console.log('Hallo Welt!');
     return 12+13;
 }
-function picture(){
-    var popup = document.getElementByClass("popup");
-    popup.classList.toggle("show");
-}
+
+// function picture(){
+//     var popup = document.getElementsByClassName("popup");
+//     popup.classList.toggle("show");
+// }
+
+
+/*alert( 'Button Down');
+    console.log('Hallo Welt!');
+    return 12+13; 
+
+    popup.classList.toggle("show"); */
