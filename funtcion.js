a552e1f03ffe356ab9dc6f4476c85174cf51037b
tim-risch <t.risch@schueler.lg-ks.de>
2025-01-29 15:16:31 +0100

    disabling rightclick, ...

--- funtcion.js
+++ funtcion.js
@@ -1,17 +1,9 @@
+document.querySelector('body').addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+})
+
 function buttonDown(){
     alert( 'Button Down');
     console.log('Hallo Welt!');
     return 12+13;
 }
-
-// function picture(){
-//     var popup = document.getElementsByClassName("popup");
-//     popup.classList.toggle("show");
-// }
-
-
-/*alert( 'Button Down');
-    console.log('Hallo Welt!');
-    return 12+13; 
-
-    popup.classList.toggle("show"); */
