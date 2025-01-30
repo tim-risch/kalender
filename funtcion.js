217797dd8de65c85aaac9f643df046c6f91ab011
tim-risch <t.risch@schueler.lg-ks.de>
2025-01-30 15:33:11 +0100

    refactor

--- funtcion.js
+++ funtcion.js
@@ -1,7 +1,15 @@
+const today= Date();
+
+// alert(today);
 document.querySelector('body').addEventListener('contextmenu', (e) => {
     e.preventDefault();
 })
 
+
+function picture(){
+    var popup = document.getElementById("popup1");
+    popup.classList.toggle("show");
+}
 function buttonDown(){
     alert( 'Button Down');
     console.log('Hallo Welt!');
